b538a2e62e1296951cdbb59d8915f3422805de13
John Ryan Camatog <john.ryan.camatog@gmail.com>
2021-09-25 11:54:54 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Farm from './components/Farm.vue';
+import WorldTree from './components/WorldTree.vue';
+
+const token = ref('');
+const hasToken = computed(() => token.value !== '')
+</script>
+
+<template>
+  <Farm :token="token" />
+  <WorldTree :token="token" />
+  
+  <input v-model="token" type="text" />
+  <div v-text="hasToken ? 'yay token' : 'oh hell nah'" />
+</template>
---
+++ src/components/WorldTree.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ token: string }>()
+</script>
+
+<template>
+  World Tree
+</template>
